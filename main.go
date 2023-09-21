f745e5399f9be1fe61116c1a2f50623023be6b58
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-21 10:01:49 -0400

    ++

--- main.go
+++ main.go
@@ -21,3 +21,27 @@ func main() {
 		Printfln("Error: %v", err.Error())
 	}
 }
+
+/*package main
+import (
+	// "fmt"
+	// "time"
+	"os"
+	"encoding/json"
+)
+func main() {
+	cheapProducts := []Product {}
+	for _, p := range Products {
+		if (p.Price < 100) {
+			cheapProducts = append(cheapProducts, p)
+		}
+	}
+	file, err := os.CreateTemp(".", "tempfile-*.json")
+	if (err == nil) {
+		defer file.Close()
+		encoder := json.NewEncoder(file)
+		encoder.Encode(cheapProducts)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}*/
