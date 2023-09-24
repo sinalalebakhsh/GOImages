275ab33058dc1032eb10f002ef5009e5b6e79bbb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-24 18:25:20 -0400

    ++

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 )
 
 func Exec(t *template.Template) error {
-	return t.Execute(os.Stdout, &Kayak)
+	return t.Execute(os.Stdout, Products)
 }
 func main() {
-	allTemplates, err := template.ParseGlob("templates/*.html")
+	
+	allTemplates, err := template.ParseFiles("templates/template.html","templates/list.html")
+
+
+
 	if err == nil {
-		selectedTemplated := allTemplates.Lookup("template.html")
+		selectedTemplated := allTemplates.Lookup("mainTemplate")
 		err = Exec(selectedTemplated)
 	}
-
-	
 	if err != nil {
 		Printfln("Error: %v %v", err.Error())
 	}
