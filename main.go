a2626833c7754362c8d2d7ec7d33fb292dce5e7e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-27 16:25:39 -0400

    ++

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
-
 import (
-	"html/template"
+	"text/template"
 	"os"
+	"strings"
 )
-
+func GetCategories(products []Product) (categories []string) {
+	catMap := map[string]string {}
+	for _, p := range products {
+		if (catMap[p.Category] == "") {
+			catMap[p.Category] = p.Category
+			categories = append(categories, p.Category)
+		}
+	}
+	return
+}
 func Exec(t *template.Template) error {
-	return t.Execute(os.Stdout, Products)
+	productMap := map[string]Product {}
+	for _, p := range Products {
+		productMap[p.Name] = p
+	}
+	return t.Execute(os.Stdout, &productMap)
 }
 func main() {
-	
-	allTemplates, err := template.ParseFiles("templates/template.html","templates/list.html")
-
-
-
-	if err == nil {
+	allTemplates := template.New("allTemplates")
+	allTemplates.Funcs(map[string]interface{} {
+		"getCats": GetCategories,
+		"lower": strings.ToLower,
+	})
+	allTemplates, err := allTemplates.ParseGlob("templates/*.txt")
+	if (err == nil) {
 		selectedTemplated := allTemplates.Lookup("mainTemplate")
 		err = Exec(selectedTemplated)
 	}
-	if err != nil {
+	if (err != nil) {
 		Printfln("Error: %v %v", err.Error())
 	}
 }
