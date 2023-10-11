c7cbebd0dd5155f87d426d1114200bffcbf288a7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-11 08:59:18 -0400

    ++

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module httpserver
+module httpclient
 
 go 1.21.0
--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
+
 import (
 	"net/http"
-	"io"
 )
-type StringHandler struct {
-	message string
-}
-func (sh StringHandler) ServeHTTP(writer http.ResponseWriter,
-		request *http.Request) {
-	Printfln("Request for %v", request.URL.Path)
-	io.WriteString(writer, sh.message)
-}
 
 func main() {
-	http.Handle("/message", StringHandler{ "Hello, World"})
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-	http.Handle("/", http.RedirectHandler("/message", http.StatusTemporaryRedirect))
-
-	fsHandler := http.FileServer(http.Dir("./static"))
-	http.Handle("/files/", http.StripPrefix("/files", fsHandler))
-
-	err := http.ListenAndServe(":5000", nil)
-	if (err != nil) {
-		Printfln("Error: %v", err.Error())
-	}
+	Printfln("Starting HTTP Server")
+	http.ListenAndServe(":5000", nil)
 }
