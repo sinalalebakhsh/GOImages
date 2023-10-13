8a72382a17ef9e227f230b1a218adc5279a90757
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-13 09:55:00 -0400

    ++

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 import (
-    "net/http"
-    "os"
-    "time"
-    "io"
-    //"encoding/json"
+	"net/http"
+	"os"
+	"time"
+	"io"
+	"encoding/json"
+	"strings"
 )
 func main() {
-    go http.ListenAndServe(":5000", nil)
-    time.Sleep(time.Second)
-    formData := map[string][]string {
-        "name":  { "Kayak "},
-        "category": { "Watersports"},
-        "price":  { "279"},
-    }
-    response, err := http.PostForm("http://localhost:5000/echo", formData)
-    if (err == nil && response.StatusCode == http.StatusOK) {
-        io.Copy(os.Stdout, response.Body)
-        defer response.Body.Close()
-    } else {
-        Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
-    }
+	go http.ListenAndServe(":5000", nil)
+	time.Sleep(time.Second)
+	var builder strings.Builder
+	err := json.NewEncoder(&builder).Encode(Products[0])
+	if (err == nil) {
+		response, err := http.Post("http://localhost:5000/echo", "application/json",strings.NewReader(builder.String()))
+		if (err == nil && response.StatusCode == http.StatusOK) {
+			io.Copy(os.Stdout, response.Body)
+			defer response.Body.Close()
+		} else {
+			Printfln("Error: %v", err.Error())
+		}
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
 }
