6db8f998c16579b8e29ce0b109a3b6ee39162fd1
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-13 09:30:54 -0400

    ++

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 import (
 	"net/http"
-	"os"
+	//"os"
 	"time"
-	"io"
+	//"io"
+	"encoding/json"
 )
 func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
-	response, err := http.Get("http://localhost:5000/html")
+	response, err := http.Get("http://localhost:5000/json")
 	if (err == nil && response.StatusCode == http.StatusOK) {
-		data, err := io.ReadAll(response.Body)
+		defer response.Body.Close()
+		data := []Product {}
+		err = json.NewDecoder(response.Body).Decode(&data)
 		if (err == nil) {
-			defer response.Body.Close()
-			os.Stdout.Write(data)
+			for _, p := range data {
+				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
+			}
+		} else {
+			Printfln("Decode error: %v", err.Error())
 		}
 	} else {
 		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
