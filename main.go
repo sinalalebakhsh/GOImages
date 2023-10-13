1463897ba437c1b4a15658d5457aaa2a527fb450
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-13 09:27:43 -0400

    ++

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
-
 import (
 	"net/http"
+	"os"
+	"time"
+	"io"
 )
-
 func main() {
-	Printfln("Starting HTTP Server")
-	http.ListenAndServe(":5000", nil)
+	go http.ListenAndServe(":5000", nil)
+	time.Sleep(time.Second)
+	response, err := http.Get("http://localhost:5000/html")
+	if (err == nil && response.StatusCode == http.StatusOK) {
+		data, err := io.ReadAll(response.Body)
+		if (err == nil) {
+			defer response.Body.Close()
+			os.Stdout.Write(data)
+		}
+	} else {
+		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+	}
 }
