ed243052adad26581128ccf4ecf9ac3f2e8e1912
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-13 09:45:06 -0400

    ++

--- main.go
+++ main.go
@@ -1,27 +1,24 @@
 package main
 import (
-	"net/http"
-	//"os"
-	"time"
-	//"io"
-	"encoding/json"
+    "net/http"
+    "os"
+    "time"
+    "io"
+    //"encoding/json"
 )
 func main() {
-	go http.ListenAndServe(":5000", nil)
-	time.Sleep(time.Second)
-	response, err := http.Get("http://localhost:5000/json")
-	if (err == nil && response.StatusCode == http.StatusOK) {
-		defer response.Body.Close()
-		data := []Product {}
-		err = json.NewDecoder(response.Body).Decode(&data)
-		if (err == nil) {
-			for _, p := range data {
-				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
-			}
-		} else {
-			Printfln("Decode error: %v", err.Error())
-		}
-	} else {
-		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
-	}
+    go http.ListenAndServe(":5000", nil)
+    time.Sleep(time.Second)
+    formData := map[string][]string {
+        "name":  { "Kayak "},
+        "category": { "Watersports"},
+        "price":  { "279"},
+    }
+    response, err := http.PostForm("http://localhost:5000/echo", formData)
+    if (err == nil && response.StatusCode == http.StatusOK) {
+        io.Copy(os.Stdout, response.Body)
+        defer response.Body.Close()
+    } else {
+        Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+    }
 }
