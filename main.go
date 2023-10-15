22fd8994199789bfc8593f58053445ae2286ba1b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-15 13:11:39 -0400

    ++

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"encoding/json"
 	"strings"
+	"net/url"
 )
 func main() {
 	go http.ListenAndServe(":5000", nil)
@@ -13,16 +14,27 @@ func main() {
 	var builder strings.Builder
 	err := json.NewEncoder(&builder).Encode(Products[0])
 	if (err == nil) {
-		response, err := http.Post("http://localhost:5000/echo",
-			"application/json",
-			strings.NewReader(builder.String()))
-		if (err == nil && response.StatusCode == http.StatusOK) {
-			io.Copy(os.Stdout, response.Body)
-			defer response.Body.Close()
+		reqURL, err := url.Parse("http://localhost:5000/echo")
+		if (err == nil) {
+			req := http.Request {
+				Method: http.MethodPost,
+				URL: reqURL,
+				Header: map[string][]string {
+					"Content-Type": { "application.json" },
+				},
+				Body: io.NopCloser(strings.NewReader(builder.String())),
+			}
+			response, err := http.DefaultClient.Do(&req)
+			if (err == nil && response.StatusCode == http.StatusOK) {
+				io.Copy(os.Stdout, response.Body)
+				defer response.Body.Close()
+			} else {
+				Printfln("Request Error: %v", err.Error())
+			}
 		} else {
-			Printfln("Error: %v", err.Error())
+			Printfln("Parse Error: %v", err.Error())
 		}
 	} else {
-		Printfln("Error: %v", err.Error())
+		Printfln("Encoder Error: %v", err.Error())
 	}
 }
