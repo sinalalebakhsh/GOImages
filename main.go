27330ad4265173f180cb34a5ef739470cbb721f6
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-16 17:22:03 -0400

    ++

--- main.go
+++ main.go
@@ -1,40 +1,18 @@
 package main
 import (
 	"net/http"
-	"os"
-	"time"
 	"io"
-	"encoding/json"
-	"strings"
-	"net/url"
 )
+type StringHandler struct {
+	message string
+}
+func (sh StringHandler) ServeHTTP(writer http.ResponseWriter,
+		request *http.Request) {
+	io.WriteString(writer, sh.message)
+}
 func main() {
-	go http.ListenAndServe(":5000", nil)
-	time.Sleep(time.Second)
-	var builder strings.Builder
-	err := json.NewEncoder(&builder).Encode(Products[0])
-	if (err == nil) {
-		reqURL, err := url.Parse("http://localhost:5000/echo")
-		if (err == nil) {
-			req := http.Request {
-				Method: http.MethodPost,
-				URL: reqURL,
-				Header: map[string][]string {
-					"Content-Type": { "application.json" },
-				},
-				Body: io.NopCloser(strings.NewReader(builder.String())),
-			}
-			response, err := http.DefaultClient.Do(&req)
-			if (err == nil && response.StatusCode == http.StatusOK) {
-				io.Copy(os.Stdout, response.Body)
-				defer response.Body.Close()
-			} else {
-				Printfln("Request Error: %v", err.Error())
-			}
-		} else {
-			Printfln("Parse Error: %v", err.Error())
-		}
-	} else {
-		Printfln("Encoder Error: %v", err.Error())
+	err := http.ListenAndServe(":5000", StringHandler{ message: "Hello, World"})
+	if (err != nil) {
+		Printfln("Error: %v", err.Error())
 	}
 }
