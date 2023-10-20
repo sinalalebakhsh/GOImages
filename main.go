d01be1d6d0fe8101523d0dd3f53e0db40118045f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-20 14:37:56 -0400

    ++

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 import (
-	"encoding/json"
 	"io"
 	"net/http"
 	"os"
-	"strings"
 	"time"
+	// "encoding/json"
+	// "strings"
 	//"net/url"
+	"net/http/cookiejar"
 )
 func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
-	var builder strings.Builder
-	err := json.NewEncoder(&builder).Encode(Products[0])
+	jar, err := cookiejar.New(nil)
 	if err == nil {
-		req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/echo",
-			io.NopCloser(strings.NewReader(builder.String())))
+		http.DefaultClient.Jar = jar
+	}
+	for i := 0; i < 3; i++ {
+		req, err := http.NewRequest(http.MethodGet,
+			"http://localhost:5000/cookie", nil)
 		if err == nil {
-			req.Header["Content-Type"] = []string{"application/json"}
 			response, err := http.DefaultClient.Do(req)
 			if err == nil && response.StatusCode == http.StatusOK {
 				io.Copy(os.Stdout, response.Body)
@@ -28,7 +30,5 @@ func main() {
 		} else {
 			Printfln("Request Init Error: %v", err.Error())
 		}
-	} else {
-		Printfln("Encoder Error: %v", err.Error())
 	}
 }
---
+++ server_cookie.go
@@ -0,0 +1,28 @@
+package main
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+func init() {
+	http.HandleFunc("/cookie",
+		func(writer http.ResponseWriter, request *http.Request) {
+			counterVal := 1
+			counterCookie, err := request.Cookie("counter")
+			if err == nil {
+				counterVal, _ = strconv.Atoi(counterCookie.Value)
+				counterVal++
+			}
+			http.SetCookie(writer, &http.Cookie{
+				Name: "counter", Value: strconv.Itoa(counterVal),
+			})
+			if len(request.Cookies()) > 0 {
+				for _, c := range request.Cookies() {
+					fmt.Fprintf(writer, "Cookie Name: %v, Value: %v\n",
+						c.Name, c.Value)
+				}
+			} else {
+				fmt.Fprintln(writer, "Request contains no cookies")
+			}
+		})
+}
