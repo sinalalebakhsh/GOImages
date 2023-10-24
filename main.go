74a2f162a892c84a8cfb627844809a01e18e4112
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-24 16:48:12 -0400

    ++

--- main.go
+++ main.go
@@ -1,40 +1,29 @@
 package main
 import (
-	"io"
 	"net/http"
 	"os"
+	"io"
 	"time"
 	//"encoding/json"
 	//"strings"
 	//"net/url"
-	"fmt"
-	"net/http/cookiejar"
+	//"net/http/cookiejar"
+	//"fmt"
 )
 func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
-	jar, err := cookiejar.New(nil)
-	clients := make([]http.Client, 3)
-	for index, client := range clients {
-		//jar, err := cookiejar.New(nil)
-		if err == nil {
-			client.Jar = jar
+	req, err := http.NewRequest(http.MethodGet,
+		"http://localhost:5000/redirect1", nil)
+	if (err == nil) {
+		var response *http.Response
+		response, err = http.DefaultClient.Do(req)
+		if (err == nil) {
+			io.Copy(os.Stdout, response.Body)
+		} else {
+			Printfln("Request Error: %v", err.Error())
 		}
-		for i := 0; i < 3; i++ {
-			req, err := http.NewRequest(http.MethodGet,
-				"http://localhost:5000/cookie", nil)
-			if err == nil {
-				response, err := client.Do(req)
-				if err == nil && response.StatusCode == http.StatusOK {
-					fmt.Fprintf(os.Stdout, "Client %v: ", index)
-					io.Copy(os.Stdout, response.Body)
-					defer response.Body.Close()
-				} else {
-					Printfln("Request Error: %v", err.Error())
-				}
-			} else {
-				Printfln("Request Init Error: %v", err.Error())
-			}
-		}
+	} else {
+		Printfln("Error: %v", err.Error())
 	}
 }
