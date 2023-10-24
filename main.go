4c402b6a59a263a81471f1628f7f8add560094a3
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-24 16:39:50 -0400

    ++

--- main.go
+++ main.go
@@ -1,34 +1,35 @@
 package main
 import (
+	"io"
 	"net/http"
 	"os"
 	"time"
-	"io"
 	//"encoding/json"
 	//"strings"
 	//"net/url"
-	"net/http/cookiejar"
 	"fmt"
+	"net/http/cookiejar"
 )
 func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
+	jar, err := cookiejar.New(nil)
 	clients := make([]http.Client, 3)
 	for index, client := range clients {
-		jar, err := cookiejar.New(nil)
-		if (err == nil) {
+		//jar, err := cookiejar.New(nil)
+		if err == nil {
 			client.Jar = jar
 		}
 		for i := 0; i < 3; i++ {
 			req, err := http.NewRequest(http.MethodGet,
 				"http://localhost:5000/cookie", nil)
-			if (err == nil) {
+			if err == nil {
 				response, err := client.Do(req)
-				if (err == nil && response.StatusCode == http.StatusOK) {
+				if err == nil && response.StatusCode == http.StatusOK {
 					fmt.Fprintf(os.Stdout, "Client %v: ", index)
 					io.Copy(os.Stdout, response.Body)
 					defer response.Body.Close()
-				}  else {
+				} else {
 					Printfln("Request Error: %v", err.Error())
 				}
 			} else {
