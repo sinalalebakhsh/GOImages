91421ba1174d756a84054494b0cc60303e1d0206
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-24 16:48:22 -0400

    ++

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ import (
 	"time"
 	//"encoding/json"
 	//"strings"
-	//"net/url"
+	"net/url"
 	//"net/http/cookiejar"
 	//"fmt"
 )
 func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
+	http.DefaultClient.CheckRedirect = func(req *http.Request,
+		previous []*http.Request) error {
+		if len(previous) == 3 {
+			url, _ := url.Parse("http://localhost:5000/html")
+			req.URL = url
+		}
+		return nil
+	}
 	req, err := http.NewRequest(http.MethodGet,
 		"http://localhost:5000/redirect1", nil)
 	if (err == nil) {
