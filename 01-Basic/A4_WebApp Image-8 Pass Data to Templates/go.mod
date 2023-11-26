52527f241b0a7ae6a1a1a3ba51ef4289001f47a7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-26 11:06:58 -0500

    A4 WebApp Image-8 Pass Data to Templates

---
+++ 01-Basic/A4_WebApp Image-8 Pass Data to Templates/go.mod
@@ -0,0 +1,3 @@
+module web3
+
+go 1.21.3
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "A4 WebApp Image-7 "
+git commit -m "A4 WebApp Image-8 Pass Data to Templates "
 git push -u origin main 
