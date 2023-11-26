1c7b7f076f52b6ecc1487a0febe4a5385c8c64dc
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-26 08:38:46 -0500

    A4 WebApp Image-6

---
+++ 01-Basic/A4_WebApp Image-6/pkg/render/render.go
@@ -0,0 +1,43 @@
+package render
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tmplCache = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+	_, inMap := tmplCache[t]
+	if !inMap {
+		err = makeTemplateCache(t)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("Template in cache")
+	}
+	tmpl = tmplCache[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func makeTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	tmplCache[t] = tmpl
+	return nil
+}
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "A4 WebApp Image-5 "
+git commit -m "A4 WebApp Image-6 "
 git push -u origin main 
