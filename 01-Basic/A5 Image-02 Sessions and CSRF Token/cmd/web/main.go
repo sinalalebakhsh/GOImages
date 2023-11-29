d1d04f84936249081fbf27d269fa117cf770b013
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-29 05:13:53 -0500

    A5 Image-01 Sessions CSRF Cross-Side Request Forgery Attacks prevention

---
+++ 01-Basic/A5 Image-02 Sessions and CSRF Token/cmd/web/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/alexedwards/scs/v2"
+	"log"
+	"net/http"
+	"time"
+	"web3/pkg/config"
+	"web3/pkg/handlers"
+)
+
+var sessionManager *scs.SessionManager
+var app config.AppConfig
+
+func main() {
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+	sessionManager.Cookie.Persist = true
+	sessionManager.Cookie.Secure = false
+	sessionManager.Cookie.SameSite = http.SameSiteLaxMode
+	app.Session = sessionManager
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: routes(&app),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	/* Previous development
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/about", handlers.AboutHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+	*/
+}
+
+
+/* installations 
+
+1-go-chi
+https://github.com/go-chi/chi#install
+	go get -u github.com/go-chi/chi/v5
+
+2-CSRF Token
+	go get github.com/justinas/nosurf
+	
+*/
+
+
+/*
+For running in current directory , Open Terminal and write:
+
+	go run ./cmd/web
+
+*/
--- U.sh
+++ U.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "A5 Image-01 Middleware "
+git commit -m "A5 Image-01 Sessions CSRF
+Cross-Side Request Forgery Attacks prevention "
 git push -u origin main 
