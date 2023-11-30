a2c6c0cd886426763671a03a210f908263a064ba
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-30 03:33:49 -0500

    A5 Image-08 Passing Form Data CSRF Token

---
+++ 01-Basic/A1-A30/A5 Image-08 Passing Form Data CSRF Token/cmd/web/routes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+	"web3/pkg/config"
+	"web3/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// Mux stand for HTTP Request Multiplexer
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(LosgRequestInfo)
+
+	mux.Use(NoSurf)
+	mux.Use(SetupSession)
+
+	mux.Get("/", handlers.Repo.HomeHandler)
+	mux.Get("/about", handlers.Repo.AboutHandler)
+	mux.Get("/login", handlers.Repo.LoginHandler)
+	mux.Get("/makepost", handlers.Repo.MakePostHandler)
+	mux.Get("/page", handlers.Repo.PageHandler)
+
+	mux.Post("/makepost", handlers.Repo.PostMakePostHandler)
+
+	return mux
+}
