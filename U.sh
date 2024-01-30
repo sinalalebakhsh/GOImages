2fdd08a56c59400973b4bb0c5a49258bb77d3f45
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-01-30 11:24:20 -0500

    01-Basic/--> Practices-00-TO-30/--> Practice-10 BEDMAS BEDMAS, standing for Brackets, Exponents, Division/Multiplication, Addition/Subtraction is common in Canada and New Zealand.

--- U.sh
+++ U.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m " 01-Basic/--> Practices-00-TO-30/--> Practice-09 Get User Input "
+git commit -m " 01-Basic/--> Practices-00-TO-30/--> Practice-10 BEDMAS
+BEDMAS, standing for Brackets, Exponents, Division/Multiplication, Addition/Subtraction is common in Canada and New Zealand."
 git push -u origin main 
